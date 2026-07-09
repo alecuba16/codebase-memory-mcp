ajamescarey-debug/MCP-the-brain

test(perl-lsp): add Perl LSP resolution suite mirroring test_php_lsp.c
Replace the plan-01 placeholder with the full Perl LSP test suite covering the
ten foundational resolution scenarios from 22-RESEARCH.md, plus the extract_perl
/ find_resolved / require_resolved / find_resolved_with_strategy helpers cloned
from test_php_lsp.c:

  1. method via bless-assignment      6. use parent MRO
  2. constructor class-method type    7. use base MRO
  3. static package call              8. Exporter import (use Mod qw(f))
  4. $self method dispatch            9. require fallback
  5. @ISA inheritance               10. unresolvable receiver -> zero edges

Assertions match the resolver's actual QN scheme (module_qn.subname, dotted —
the Perl package governs dispatch, not the emitted QN), and the negative test
confirms the zero-edge guarantee for untyped scalar and unindexed package
receivers. Register suite_perl_lsp in test_main.c so scripts/test.sh runs it.

All 10 perl_lsp tests pass (3563 passed, 1 pre-existing unrelated failure:
search_code_multi_word in tests/test_mcp.c).

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>
Signed-off-by: Shane McCarron <shane.mccarron@corvexconnect.com>

tests/test_main.c
@@ -50,6 +50,7 @@ extern void suite_c_lsp(void);
 extern void suite_php_lsp(void);
 extern void suite_cs_lsp(void);
 extern void suite_cs_lsp_bench(void);
+extern void suite_perl_lsp(void);
 extern void suite_scope(void);
 extern void suite_type_rep(void);
 extern void suite_py_lsp(void);
@@ -172,6 +173,7 @@ int main(void) {
     RUN_SUITE(php_lsp);
     RUN_SUITE(cs_lsp);
     RUN_SUITE(cs_lsp_bench);
+    RUN_SUITE(perl_lsp);
     RUN_SUITE(py_lsp);
     RUN_SUITE(kotlin_lsp);
     RUN_SUITE(rust_lsp);

alecuba16/codebase-memory-mcp

tests/test_main.c
@@ -187,6 +187,7 @@ extern void suite_c_lsp(void);
 extern void suite_php_lsp(void);
 extern void suite_cs_lsp(void);
 extern void suite_cs_lsp_bench(void);
+extern void suite_perl_lsp(void);
 extern void suite_scope(void);
 extern void suite_type_rep(void);
 extern void suite_py_lsp(void);
@@ -344,6 +345,7 @@ int main(int argc, char **argv) {
     RUN_SELECTED_SUITE(php_lsp);
     RUN_SELECTED_SUITE(cs_lsp);
     RUN_SELECTED_SUITE(cs_lsp_bench);
+    RUN_SELECTED_SUITE(perl_lsp);
     RUN_SELECTED_SUITE(py_lsp);
     RUN_SELECTED_SUITE(kotlin_lsp);
     RUN_SELECTED_SUITE(rust_lsp);